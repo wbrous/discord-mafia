3f32109678debc1903b076be9df81edba38fb8ed
redisnotbluedev <ededmaude@gmail.com>
2026-01-05 09:35:38 +1030

    remember, even after death

--- classes/abstractor.py
+++ classes/abstractor.py
@@ -1,4 +1,4 @@
-import discord, logging
+import discord, logging, data
 from classes.views import StartGameView
 
 logger = logging.getLogger(__name__)
@@ -10,6 +10,8 @@ class GameAbstractor:
 
 	def __init__(self, channel: int):
 		self.channel = channel
+		config = data.load()
+		self.last_lobby = config.get("last_lobbies", {self.channel: None})[self.channel]
 	
 	async def on_message(self, message: discord.Message):
 		if message.channel.id != self.channel: return
@@ -23,5 +25,11 @@ class GameAbstractor:
 				description="The series by Turing Games, now as a Discord bot!",
 				color=discord.Color.blurple()
 			), view=StartGameView(self))
+
+			config = data.load()
+			if "last_lobbies" in config:
+				config.last_lobbies[self.channel] = self.last_lobby.id
+			else:
+				config["last_lobbies"] = {self.channel: self.last_lobby.id}
 		else:
 			logger.info("Skipping message send as the game is already running!")
