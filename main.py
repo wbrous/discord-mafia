30e9c0b195e6487a06ba4f94e073cf98d2ad1e13
redisnotbluedev <ededmaude@gmail.com>
2026-01-04 15:21:09 +1030

    remove the code

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
-import discord, os
+import discord, os, logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Define intents
 intents = discord.Intents.default()
-# Enable the MESSAGE CONTENT intent for the bot to read message content
 intents.message_content = True 
+bot = discord.Client(intents=intents)
 
-# Create an instance of a Client with specified intents
-client = discord.Client(intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-	"""Called when the bot has finished logging in and setting things up."""
-	print(f"{client.user} has connected to Discord!")
-	print("------")
+	pass
 
-@client.event
+@bot.event
 async def on_message(message):
-	"""Called when a message is received."""
-	if message.author == client.user:
+	if message.author == bot.user:
 		return
 
 	if message.content.startswith("$hello"):
 		await message.channel.send("Hello!")
 
 TOKEN = os.getenv("TOKEN")
-client.run(TOKEN)
+bot.run(TOKEN)
