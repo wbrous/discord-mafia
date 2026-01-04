0f37a00f292001be96f97fd4dd666ff8b40a0d63
redisnotbluedev <ededmaude@gmail.com>
2026-01-05 09:48:00 +1030

    ok surely this time

--- classes/abstractor.py
+++ classes/abstractor.py
@@ -8,29 +8,32 @@ class GameAbstractor:
 	running = False
 	last_lobby: discord.Message = None
 
-	def __init__(self, channel: str):
+	def __init__(self, channel: int):
 		self.channel = channel
+		self.channel_key = str(channel)
 		config = data.load()
-		self.last_lobby = config.get("last_lobbies", {}).get(self.channel, None)
-	
+		self.last_lobby = config.get("last_lobbies", {}).get(self.channel_key, None)
+
 	async def on_message(self, message: discord.Message):
-		if message.channel.id != int(self.channel): return
-		
+		if message.channel.id != self.channel:
+			return
+
 		if not self.running:
 			if self.last_lobby:
 				await self.last_lobby.delete()
-			
-			self.last_lobby = await message.channel.send(embed=discord.Embed(
-				title="AI Plays Mafia",
-				description="The series by Turing Games, now as a Discord bot!",
-				color=discord.Color.blurple()
-			), view=StartGameView(self))
+
+			self.last_lobby = await message.channel.send(
+				embed=discord.Embed(
+					title="AI Plays Mafia",
+					description="The series by Turing Games, now as a Discord bot!",
+					color=discord.Color.blurple(),
+				),
+				view=StartGameView(self),
+			)
 
 			config = data.load()
-			if "last_lobbies" in config:
-				config["last_lobbies"][self.channel] = self.last_lobby.id
-			else:
-				config["last_lobbies"] = {self.channel: self.last_lobby.id}
+			last_lobbies = config.setdefault("last_lobbies", {})
+			last_lobbies[self.channel_key] = self.last_lobby.id
 			data.save(config)
 		else:
 			logger.info("Skipping message send as the game is already running!")
