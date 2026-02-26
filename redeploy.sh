759afeff131d362962d09ae16170b71c9409bef6
redisnotbluedev <ededmaude@gmail.com>
2026-02-26 13:31:18 +1030

    wait until all games are done to redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-cd /home/arduino/discord-mafia
+
+cd $HOME/discord-mafia
 git fetch --all
 git reset --hard origin/main
+
+STATUS_FILE="games_ongoing.txt"
+
+# Check if there are any ongoing games and wait if necessary
+if [ -f "$STATUS_FILE" ]; then
+    while [ "$(cat "$STATUS_FILE")" == "1" ]; do
+        echo "Games are currently ongoing. Waiting for them to finish before redeploying..."
+        sleep 10
+    done
+fi
+
+echo "No games ongoing. Proceeding with restart."
 sudo systemctl restart mafia-bot
